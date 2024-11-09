3109ebe0bba0ea5cacade86d2b3bf394f1c58f09
boladeE <babawale030@gmail.com>
2024-11-09 17:32:28 +0100

    Stopping Point

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
 # Create your models here.
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
+def home_page(request):
+    autumn = Post.objects.all()
+    return render(request, 'blog_app/index.html', {'posts': autumn})
+
+def user_input(request):
+    if request.method == 'POST':
+        print(request.POST.get('Input 1'))
+        print(request.POST.get('Input 2'))
+    return render(request, 'blog_app/user_input.html')
 # Create your views here.
